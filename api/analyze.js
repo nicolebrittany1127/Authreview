1b6eef39483c23f05e3451b3ee5301d14ea98fa9
nicolebrittany1127 <nicole.brittany1127@gmail.com>
2026-04-17 15:51:37 -0400

    Update analyze.js

--- api/analyze.js
+++ api/analyze.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+const handler = async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
@@ -49,4 +49,6 @@ export default async function handler(req, res) {
   } catch (err) {
     return res.status(500).json({ error: 'Internal server error: ' + err.message });
   }
-}
+};
+
+module.exports = handler;
